249b4fe6e520265f6d7d1cb2f137a501aaacf58d
Nick Van Deren <email": null>
2017-10-27 16:23:36 -0700

    user login spec redirects on good login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_username(params[:username])
-    if user && user.authentication(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,4 +7,20 @@ describe SessionsController, type: :controller do
       expect(response.status).to eq 200
     end
   end
+
+  describe 'sessions#create' do
+
+    context 'good login' do
+      let!(:user){User.create!(username: 'Clera', email: 'apples@orchard.com', password: 'pear', bio: 'I love apple orchards, apple sauce, and roasted beets!')}
+      before(:each){ post :create, params: {:username => user.username, :password => 'pear'}, session: {user_id: user.id}}
+
+      it 'redirects to root on succesful login' do
+        expect(response).to redirect_to '/'
+      end
+    end
+
+    context 'bad login' do
+    end
+
+  end
 end
