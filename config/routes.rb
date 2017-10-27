f4096b1f19400697372e7ac9203b1ac1f45f2659
John Simoni <email": null>
2017-10-27 15:32:42 -0700

    added templates route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :templates
   get '/users/:id', to: 'users#show'
 end
