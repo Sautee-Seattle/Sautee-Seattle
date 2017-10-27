31f951d39bf98041abfab1f06e6f4c61478e09ea
John Simoni <email": null>
2017-10-27 15:31:39 -0700

    Removed junk User class from UsersControlle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,4 @@ class UsersController < ApplicationController
     @user = User.new
     render :show
   end
-
-  class User
-    def posts
-      posts = []
-      5.times do
-        posts << "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      end
-      posts
-    end
-  end
 end
