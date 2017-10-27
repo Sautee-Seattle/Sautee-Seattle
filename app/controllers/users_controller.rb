0af9f2ab7b2aa3ce0da56c5989a7a5942296eda1
Marlo Ito <email": null>
2017-10-27 15:29:59 -0700

    adds user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
   def new
-    @errors = ['cow', 'pie']
     @user = User.new
   end
 
   def create
-    @errors = ['hi', 'there']
-    render partial: 'shared/errors'
+    user = User.new(user_params)
+
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      @errors = user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :bio)
   end
 end
