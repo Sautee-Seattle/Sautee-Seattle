893a1bc2e515a2908bfe2671f485fba23a9baf36
John Simoni <email": null>
2017-10-27 15:25:22 -0700

    100% test coverage

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe "#show" do
+    before(:each) {get :show, params: {id: 4}}
     it "returns 200" do
-      get :show, params: {id: 4}
       expect(response).to be_ok
+    end
+    it "TEMPORARY - assigns a user instance variable with 5 posts" do
+      expect(assigns[:user].posts.length).to eq(5)
     end
   end
 end
