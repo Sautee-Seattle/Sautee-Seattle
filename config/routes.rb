c7339a689a9dc37f0413235cf198216475cd7334
Nick Van Deren <email": null>
2017-10-27 15:44:42 -0700

    adds sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'home#index'
   resources :users, only: [:new, :create]
   resources :templates
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
