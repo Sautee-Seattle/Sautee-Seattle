7aede8e850961b908246a63286f52d07ce84efbb
Marlo Ito <email": null>
2017-10-27 15:52:45 -0700

    error handling works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
-      @errors = user.errors.full_messages
+      @errors = @user.errors.full_messages
       render :new
     end
   end
