ffa0dd29c9c5800b370d9ba9e8d021c148a52b46
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-27 15:24:50 -0700

    added templates_controller to simplecov filter list

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,7 @@ SimpleCov.start 'rails' do
   add_filter "app/mailers"
   add_filter "app/models/application_record.rb"
   add_filter "app/helpers/application_helper.rb"
+  add_filter "app/controllers/templates_controller.rb"
 end
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
