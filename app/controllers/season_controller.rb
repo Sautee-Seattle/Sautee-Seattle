85f6422d82eb49a50fd20d446d31706e95deb9c2
Mandil <email": null>
2017-10-27 15:08:18 -0700

    create central main-page nav

--- app/controllers/season_controller.rb
+++
@@ -1,9 +0,0 @@
-class SeasonController < ApplicationController
-
-  def index
-  end
-
-  def show
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :seasons , only: [:index, :show]
+
+  root to: "seasons#index"
 end
