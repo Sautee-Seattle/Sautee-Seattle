4ea8ee3a6c50c993f133d519e7cf60f72103fa8c
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-27 12:48:56 -0700

    added minor logic to season controller, added basic html and season interpolation to season show page

--- app/controllers/season_controller.rb
+++ app/controllers/season_controller.rb
@@ -4,6 +4,10 @@ class SeasonController < ApplicationController
   end
 
   def show
+    #Create logic to detemine which season is given and set CSS and produce to aforementioned season
+    Season.create!(name: "Fall")
+    season = Season.last
+    # season = Season.all
   end
 
 end
