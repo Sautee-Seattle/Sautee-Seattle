867a55b7a0717ca29d61bdd1fe25f78f3d12fd9e
John Simoni <email": null>
2017-10-28 11:55:39 -0700

    Added association spec for IngredientsPosts

--- spec/models/ingredients_post_spec.rb
+++ spec/models/ingredients_post_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe IngredientsPost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:ingredient) }
+  it { should belong_to(:post) }
 end
