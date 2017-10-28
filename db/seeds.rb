1e9e5428348acea4cb850eacbee44a58001218e4
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-28 13:58:25 -0700

    refactorered seed file again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,37 @@
 user = User.create!(username: 'Clera', email: 'apples@orchard.com', password: 'pear', bio: 'I love apple orchards, apple sauce, and roasted beets!')
 
-seasons = []
-seasons << Season.create!(name: "Winter")
-seasons << Season.create!(name: "Spring")
-seasons << Season.create!(name: "Summer")
-seasons << Season.create!(name: "Fall")
+all_seasons = []
+all_seasons << Season.create!(name: "Winter")
+all_seasons << Season.create!(name: "Spring")
+all_seasons << Season.create!(name: "Summer")
+all_seasons << Season.create!(name: "Fall")
 
 apple = Ingredient.create!(name: "Apple")
-  seasons.each do |season|
+  all_seasons.each do |season|
     apple.seasons << season
   end
 
 avocado = Ingredient.create!(name: "Avocado")
-  avocado.each do |season|
-    apple.seasons << season
+  all_seasons.each do |season|
+    avocado.seasons << season
   end
 
 banana = Ingredient.create!(name: "Banana")
-  seasons.each do |season|
+  all_seasons.each do |season|
     banana.seasons << season
   end
 
 beets = Ingredient.create!(name: "Beets") # Nicks Fav!
-  seasons.each do |season|
+  all_seasons.each do |season|
     beets.seasons << season
   end
 
 celery = Ingredient.create!(name: "Celery")
-  seasons.each do |season|
+  all_seasons.each do |season|
     celery.seasons << season
   end
 
 mushrooms = Ingredient.create!(name: "Mushrooms")
-  seasons.each do |season|
+  all_seasons.each do |season|
     mushrooms.seasons << season
   end
