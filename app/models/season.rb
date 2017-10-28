d7c670f462f8c312768f4c12942dd69d26998ebf
John Simoni <email": null>
2017-10-28 15:02:24 -0700

    Added season factory

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,7 +1,6 @@
 class Season < ApplicationRecord
   has_many :seasons_ingredients
   has_many :ingredients, through: :seasons_ingredients
+  
   validates :name, presence: true
-  has_many :seasons_ingredients
-  has_many :ingredients, through: :seasons_ingredients
 end
