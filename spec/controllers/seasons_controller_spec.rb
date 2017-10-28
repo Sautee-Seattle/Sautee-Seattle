4bcf6b4d4d180dd86161e36771752933bb14ad80
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-28 12:26:43 -0700

    Created 4 passing seasons controller tests based on which season was openend

--- spec/controllers/seasons_controller_spec.rb
+++ spec/controllers/seasons_controller_spec.rb
@@ -4,9 +4,21 @@ RSpec.describe SeasonsController, type: :controller do
   let!(:season) {Season.create(name: "Fall")}
 
   context "seasons#show" do
+    it "renders Winter CSS if successful" do
+      get :show, params: {id: 1}
+      expect(response.status).to eq 200
+    end
+    it "renders Spring CSS if successful" do
+      get :show, params: {id: 2}
+      expect(response.status).to eq 200
+    end
+    it "renders Summer CSS if successful" do
+      get :show, params: {id: 3}
+      expect(response.status).to eq 200
+    end
     it "renders Fall CSS if successful" do
-      get :show
-      expect(response).to include("seasons-show")
+      get :show, params: {id: 4}
+      expect(response.status).to eq 200
     end
   end
 
