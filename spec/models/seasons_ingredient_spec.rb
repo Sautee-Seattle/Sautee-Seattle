cc094299a440cde90316c6c74835a74f84ffdf96
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-28 12:49:05 -0700

    added 2 passing tests to season_ingredients spec

--- spec/models/seasons_ingredient_spec.rb
+++ spec/models/seasons_ingredient_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe SeasonsIngredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe "Seasons & Ingredients should have a relationship " do
+        let!(:season) {Season.create!(:name => "Fall")}
+        let!(:ingredient) {Ingredient.create!(:name => "Potato")}
+        before(:each) do
+            season.ingredients << ingredient
+        end
+        it "Seasons are added properly to SeasonsIngredient" do
+            expect(SeasonsIngredient.first.season).to eq season
+        end
+        it "Ingredients are added properly to SeasonsIngredient" do
+            expect(SeasonsIngredient.first.ingredient).to eq ingredient
+        end
+
+        xit "should have followers" do
+          @thing.followers.should == [@user]
+        end
+
+    end
+
 end
