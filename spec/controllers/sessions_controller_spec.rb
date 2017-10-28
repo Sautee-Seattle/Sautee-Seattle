5392eb40ee81d08414dbd6fcd3d9015f2c758d32
Nick Van Deren <email": null>
2017-10-27 17:04:18 -0700

    creating login logout routes

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe SessionsController, type: :controller do
+  let!(:user){User.create!(username: 'Clera', email: 'apples@orchard.com', password: 'pear', bio: 'I love apple orchards, apple sauce, and roasted beets!')}
+  let(:session){Hash.new}
+
   describe 'sessions#new' do
     it 'returns a 200 status' do
       get :new
@@ -9,9 +12,7 @@ describe SessionsController, type: :controller do
   end
 
   describe 'sessions#create' do
-
     context 'good login' do
-      let!(:user){User.create!(username: 'Clera', email: 'apples@orchard.com', password: 'pear', bio: 'I love apple orchards, apple sauce, and roasted beets!')}
       before(:each){ post :create, params: {:username => user.username, :password => 'pear'}, session: {user_id: user.id}}
 
       it 'redirects to root on succesful login' do
@@ -20,7 +21,18 @@ describe SessionsController, type: :controller do
     end
 
     context 'bad login' do
+      before(:each){ post :create, params: {:username => user.username, :password => 'soup'}, session: {user_id: user.id}}
+
+      it 'redirects to login on a failed login' do
+        expect(response).to redirect_to '/login'
+      end
     end
 
+    describe "sessions#destroy" do
+      it 'destroys the session' do
+        delete :destroy, session: {user_id: user.id}
+        expect(session[:user_id]).to be_nil
+      end
+    end
   end
 end
