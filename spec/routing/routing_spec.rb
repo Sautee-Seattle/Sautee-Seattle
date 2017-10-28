a9a751f7ebd60e99c1ba7e73124616987b15eb17
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-28 12:16:46 -0700

    added passing test for seasons index to routing tests

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -2,21 +2,11 @@
 require 'rails_helper'
 
 describe "Routing", type: :controller do
-  # context "Sessions Routing" do
-  #   it "routes GET /login to sessions#new" do
-  #     expect(:get => '/login').to route_to('sessions#new')
-  #   end
-  #   it "routes POST /login to sessions#create" do
-  #     expect(:post => '/login').to route_to('sessions#create')
-  #   end
-  # end
 
   context "Seasons Routing" do
-    it "route GET /index to seasons#index" do
+    it "route GET /seasons to seasons#index" do
       expect(:get => '/seasons').to route_to('seasons#index')
     end
   end
-
-
 
 end
