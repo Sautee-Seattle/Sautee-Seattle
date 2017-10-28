8251fcbc61f3bb7378c2fb1cf2f9685816b8367b
John Simoni <email": null>
2017-10-28 11:47:35 -0700

    Assign instance variables locations and recipes from Ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,7 @@
 class IngredientsController < ApplicationController
   def show
-    
+    @ingredient = Ingredient.find(params[:id])
+    @locations = @ingredient.posts.where("type = 'location'")
+    @recipes = @ingredient.posts.where("type = 'recipe'")
   end
 end
