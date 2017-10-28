d5b0fafea2cd1f6c653581f18d0cb1acfa45e01e
John Simoni <jsimoni03@gmail.com>
2017-10-28 10:27:02 -0700

    Started creating ingredients factory and added tests to ingredients controller spec

--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe IngredientsController, type: :controller do
+  describe "#show" do
+    let(:ingredient) { create(:ingredient) }
+    before(:each) { get :show, params: {id: ingredient.id} }
 
+    it "returns 200" do
+      expect(response).to be_ok
+    end
+
+    it "assigns a locations instance variable to an array" do
+      expect(assigns[:locations].kind_of?(Array)).to eq(true)
+    end
+
+    it "assings a recipes instance variable to an array" do
+      expect(assigns[:recipes].kind_of?(Array)).to eq(true)
+    end
+  end
 end
---
+++ spec/factories/ingredients_factory.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :ingredient do
+    name 'cofeve'
+  end
+end
