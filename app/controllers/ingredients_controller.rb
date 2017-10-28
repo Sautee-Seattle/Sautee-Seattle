22d4a51e6bf23a1da1b530ebb2c1355d9b00e10c
John Simoni <email": null>
2017-10-28 13:10:28 -0700

    Modified seedfile

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,7 +1,7 @@
 class IngredientsController < ApplicationController
   def show
-    # @ingredient = Ingredient.find(params[:id])
-    # @locations = @ingredient.posts.where("post_type = 'location'")
-    # @recipes = @ingredient.posts.where("post_type = 'recipe'")
+    @ingredient = Ingredient.find(params[:id])
+    @locations = @ingredient.posts.where("post_type = 'location'")
+    @recipes = @ingredient.posts.where("post_type = 'recipe'")
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
 User.create!(username: 'Clera', email: 'apples@orchard.com', password: 'pear', bio: 'I love apple orchards, apple sauce, and roasted beets!')
 
-Ingredient.create!(name: "Beet", description: 'The beetroot is the taproot portion of the beet plant,[1] usually known in North America as the beet, also table beet, garden beet, red beet, or golden beet. It is one of several of the cultivated varieties of Beta vulgaris grown for their edible taproots and their leaves (called beet greens). These varieties have been classified as B. vulgaris subsp. vulgaris Conditiva Group.')
+ing = Ingredient.create!(name: "Beet", description: 'The beetroot is the taproot portion of the beet plant,[1] usually known in North America as the beet, also table beet, garden beet, red beet, or golden beet. It is one of several of the cultivated varieties of Beta vulgaris grown for their edible taproots and their leaves (called beet greens). These varieties have been classified as B. vulgaris subsp. vulgaris Conditiva Group.')
+
+5.times do
+  ing.posts << Post.create(post_type: "location", title: "Buy Beets Here", body: "111 Street Street")
+  ing.posts << Post.create(post_type: "recipe", title: "Make Beets", body: "Beat beets until beaten. Then bring beats to bob.")  
+end
