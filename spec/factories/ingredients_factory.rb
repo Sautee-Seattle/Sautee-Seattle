2272808f9b89d185d98bf6a1e0c0b80923f000be
John Simoni <email": null>
2017-10-28 11:24:34 -0700

    Added description to ingredient factory

--- spec/factories/ingredients_factory.rb
+++ spec/factories/ingredients_factory.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :ingredient do
-    name 'cofeve'
+    name 'covfefe'
+    description 'The mighty and terrible god of internet typos. The mysterious and all-knowing overlord of the Twitter trolls, and Pepe is his prophet. On this the 30th day of May, he was summoned by the orange king to begin his reign of terror over the lawless wasteland of cyberspace. And it shall be that no blogger, no profile, no Twitter egg shall know peace in the hereafter until they have bowed in reverence and fear at the glorious might of Covfefe.'
   end
 end
