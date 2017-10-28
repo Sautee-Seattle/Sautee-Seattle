c74c40e5716729f3c0f76d4062777bdf662d0802
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-28 13:53:59 -0700

    altered controller routes for seasons show page

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -4,7 +4,9 @@ class SeasonsController < ApplicationController
   end
 
   def show
-    @season = Season.find_by_name(params[:id])
+    season_name = params[:id]
+    season_name[0] = season_name[0].upcase
+    @season = Season.find_by_name(season_name)
   end
 
 end
