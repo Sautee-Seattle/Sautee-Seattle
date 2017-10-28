4061526f83bd201a33c501309da9d290da7d14c8
Marlo Ito <email": null>
2017-10-28 15:18:23 -0700

    adds selenium webdriver and works -- brew install chromedriver

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,43 @@
-user = User.create!(username: 'Clera', email: 'apples@orchard.com', password: 'pear', bio: 'I love apple orchards, apple sauce, and roasted beets!')
+# user = User.create!(username: 'Clera', email: 'apples@orchard.com', password: 'pear', bio: 'I love apple orchards, apple sauce, and roasted beets!')
 
-all_seasons = []
-all_seasons << Season.create!(name: "Winter")
-all_seasons << Season.create!(name: "Spring")
-all_seasons << Season.create!(name: "Summer")
-all_seasons << Season.create!(name: "Fall")
+# Selenium::WebDriver::Chrome.driver_path = <path to chromedriver> 
 
-apple = Ingredient.create!(name: "Apple")
-  all_seasons.each do |season|
-    apple.seasons << season
-  end
+driver = Selenium::WebDriver.for :chrome
 
-avocado = Ingredient.create!(name: "Avocado")
-  all_seasons.each do |season|
-    avocado.seasons << season
-  end
+driver.navigate.to "http://www.simplesteps.org/eat-local/state/washington"
 
-banana = Ingredient.create!(name: "Banana")
-  all_seasons.each do |season|
-    banana.seasons << season
-  end
-
-beets = Ingredient.create!(name: "Beets") # Nicks Fav!
-  all_seasons.each do |season|
-    beets.seasons << season
-  end
-
-celery = Ingredient.create!(name: "Celery")
-  all_seasons.each do |season|
-    celery.seasons << season
-  end
-
-mushrooms = Ingredient.create!(name: "Mushrooms")
-  all_seasons.each do |season|
-    mushrooms.seasons << season
-  end
+# all_seasons = []
+# all_seasons << Season.create!(name: "Winter")
+# all_seasons << Season.create!(name: "Spring")
+# all_seasons << Season.create!(name: "Summer")
+# all_seasons << Season.create!(name: "Fall")
+#
+# apple = Ingredient.create!(name: "Apple")
+#   all_seasons.each do |season|
+#     apple.seasons << season
+#   end
+#
+# avocado = Ingredient.create!(name: "Avocado")
+#   all_seasons.each do |season|
+#     avocado.seasons << season
+#   end
+#
+# banana = Ingredient.create!(name: "Banana")
+#   all_seasons.each do |season|
+#     banana.seasons << season
+#   end
+#
+# beets = Ingredient.create!(name: "Beets") # Nicks Fav!
+#   all_seasons.each do |season|
+#     beets.seasons << season
+#   end
+#
+# celery = Ingredient.create!(name: "Celery")
+#   all_seasons.each do |season|
+#     celery.seasons << season
+#   end
+#
+# mushrooms = Ingredient.create!(name: "Mushrooms")
+#   all_seasons.each do |season|
+#     mushrooms.seasons << season
+# end
