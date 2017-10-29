dcba56c366d08ecd78186561a6d54a7e77d5d75e
Mandil <email": null>
2017-10-29 14:48:37 -0700

    post#create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-user = User.create(username: 'Clera', email: 'apples@orchard.com', password: 'pear', bio: 'I love apple orchards, apple sauce, and roasted beets!')
-
 ################################################################################################################################################################
 # DO NOT CHANGE THIS SECTION
 def clean(produce)
@@ -16,35 +14,16 @@ end
 
 def associate_produce(produce_data, season)
   produce_data.each do |name|
-    season.ingredients << Ingredient.find_by_name(name)
+    season.ingredients << Ingredient.find_by!(name: name)
   end
 end
 
+# imports produce data from JSON file
+file = File.read('db/produce_data.json')
+produce_data = JSON.parse(file)
+
 while Season.all.length < 4
-  driver = Selenium::WebDriver.for :chrome
-  driver.navigate.to "http://www.simplesteps.org/eat-local/state/washington"
-  wait = Selenium::WebDriver::Wait.new(:timeout => 20)
-
-  produce_data = {}
-
-  # gets all seasons online
-  seasons = wait.until {
-    element_1 = driver.find_element(:class, 'state-produce')
-    element_1.find_elements(:class, 'season')
-  }
-
-  # sets produce_data hash
-  seasons.each do |season|
-    monthy = season.find_element(:tag_name, 'h3').text
-
-    season_produce = season.find_elements(:tag_name, 'a')
-
-    season_produce.map! { |item| item.text }
-
-    produce_data[monthy] = season_produce
-  end
-
-  ###############################################################################
+  ############################################################################
   # SEEDS seasons & ingredients
   winter = Season.create(name: 'Winter')
   spring = Season.create(name: 'Spring')
@@ -55,9 +34,9 @@ while Season.all.length < 4
   clean(ingredients)
 
   ingredients.each do |ingredient|
-    Ingredient.create(name: ingredient)
+    Ingredient.create!(name: ingredient)
   end
-  ###############################################################################
+  #############################################################################
 
   # gets unique produce for our 4 seasons (as opposed to bi-monthly setup online)
   winter_produce_data = get_season_produce(produce_data, 0, 1, 2, 3, 22, 23)
@@ -70,8 +49,7 @@ while Season.all.length < 4
   associate_produce(spring_produce_data, spring)
   associate_produce(summer_produce_data, summer)
   associate_produce(fall_produce_data, fall)
-
-
-  driver.quit
 end
 ################################################################################################################################################################
+
+user = User.create(username: 'Clera', email: 'apples@orchard.com', password: 'pear', bio: 'I love apple orchards, apple sauce, and roasted beets!')
