2a90df62d2358f34242383f5345c602dbe198b62
John Simoni <email": null>
2017-10-29 13:36:40 -0700

    Started adding content to ingredients/recipe index, linked to ingredient#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @ingredient = Ingredient.find(params[:ingredient_id])
+    @description = "A little bird told us that you like #{@ingredient.name.pluralize(2)}. So, we decided to put together a few of our favorite #{@ingredient.name} recipes for you to check out! YOLO."
     render :index
   end
 
