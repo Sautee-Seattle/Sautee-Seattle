9cba97f0faebdeeaf348dcd827e5d5d2e3852ab7
John Simoni <email": null>
2017-10-28 20:12:04 -0700

    Added route to create a new location

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
   resources :seasons , only: [:index, :show]
-  resources :ingredients, only: [:show]
+  resources :ingredients, only: [:show] do
+    resources :posts, only: [:create, :show] do
+      get 'location', on: :new
+    end
+  end
 
   resources :templates
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-
 end
