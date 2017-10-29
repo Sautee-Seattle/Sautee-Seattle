f2a2f1afc0a2da3a128e65eb4b808a0b4a6dc560
John Simoni <email": null>
2017-10-28 22:35:42 -0700

    Create location spec for posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -7,8 +7,39 @@ RSpec.describe PostsController, type: :controller do
     it "renders a location page" do
       expect(response).to render_template(:new_location)
     end
-    it "assings an ingredient instance variable" do
-      expect(assigns[:ingredient]).to eq(ingredient)
+  end
+
+  describe "create location" do
+    describe "valid input" do
+      before(:each) do
+        new_post = {post_type: 'location', title: 'Wenistown', body: "444 Wenis Way"}
+        post :create, params: { ingredient_id: ingredient.id, post: new_post }
+      end
+
+      it "redirects to ingredient#show" do
+        expect(response).to redirect_to(ingredient_path(ingredient))
+      end
+
+      it "creates a new location" do
+        expect(Post.all.last.title).to eq("Wenistown")
+      end
+    end
+
+    describe "invalid input" do
+      before(:each) do
+        # Should fail uniqueness validator for title
+        old_post = create(:location_with_ingredients)
+        bad_post = {post_type: 'location', title: old_post.title, body: "444 Wenis Way"}
+        post :create, params: { ingredient_id: ingredient.id, post: bad_post }
+      end
+
+      it "renders new location form" do
+        expect(response).to render_template(:new_location)
+      end
+
+      it "populates an errors instance variable" do
+        expect(assigns[:errors].length).to_not eq(0)
+      end
     end
   end
 end
