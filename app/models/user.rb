03faef1132fb948c18e9bdff87f65a16cec12525
John Simoni <email": null>
2017-10-28 20:48:09 -0700

    Refactored user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
 
   validates :username, :email, { presence: true, uniqueness: true }
   validates :password_digest, { presence: true }
+
+  def recipes
+    posts.select {|post| post.post_type == "recipe"}
+  end
+
+  def locations
+    posts.select {|post| post.post_type == "location"}
+  end
 end
