f0f73b215d824311ce2ea38214c42b453e42faaa
Marlo Ito <email": null>
2017-10-28 22:07:19 -0700

    adds get_season_data method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,17 @@
 # user = User.create!(username: 'Clera', email: 'apples@orchard.com', password: 'pear', bio: 'I love apple orchards, apple sauce, and roasted beets!')
 
 # Selenium::WebDriver::Chrome.driver_path = <path to chromedriver>
+def clean(produce)
+  produce.reject! { |x| x == "Wreathes" }
+  produce.reject! { |x| x == "Christmas Trees" }
+end
+
 
 driver = Selenium::WebDriver.for :chrome
 driver.navigate.to "http://www.simplesteps.org/eat-local/state/washington"
 wait = Selenium::WebDriver::Wait.new(:timeout => 20)
 
-produce = {}
+produce_data = {}
 
 # gets all seasons online
 seasons = wait.until {
@@ -23,16 +28,49 @@ seasons.each do |season|
 
   season_produce.map! { |item| item.text }
 
-  produce[monthy] = season_produce
+  produce_data[monthy] = season_produce
 end
 
-# example = driver.find_element(:class, 'season').find_elements(:tag_name, 'a')
+# p produce_data.values[0]
 
-# example.each { |pro| puts pro.text }
+# winter = produce_data.values[22] | produce_data.values[23] | produce_data.values[0] | produce_data.values[1] | produce_data.values[2] | produce_data.values[3]
+#
+# clean(winter)
+
+def get_season_produce(data, a, b, c, d, e, f)
+  season = data.values[a] | data.values[b] | data.values[c] | data.values[d] | data.values[e] | data.values[f]
+
+  clean(season)
+end
+
+winter = get_season_produce(produce_data, 0, 1, 2, 3, 22, 23)
+
+p winter
+
+# spring =
+#
+# summer =
+#
+# fall =
 
 
-puts produce
 
+
+
+
+
+
+
+
+################################################################################
+# ingredients = produce_data.values.flatten.uniq
+#
+# clean(ingredients)
+
+# ingredients.each do |ingredient|
+#   Ingredient.create(name: ingredient)
+# end
+################################################################################
 
 # all_seasons = []
 # all_seasons << Season.create!(name: "Winter")
