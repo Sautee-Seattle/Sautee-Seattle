f9c34e081f00e03751aaeef06f947579d851fef0
John Simoni <email": null>
2017-10-29 15:09:10 -0700

    Added spec to post model, check presence and default value of url

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,7 +5,21 @@ RSpec.describe Post, type: :model do
     it { should validate_presence_of(:post_type) }
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
+    it { should validate_presence_of(:url) }
     it { should validate_uniqueness_of(:title).scoped_to(:post_type) }
+  end
+
+  describe "hooks" do
+    let (:user) { create(:user) }
+    it "should default url to # if post_type is location" do
+      post = create(:post, post_type: "location")
+      expect(post.url).to eq("#")
+    end
+
+    it "should default url to pan.png if post_type is recipe" do
+      post = create(:post, post_type: "recipe")
+      expect(post.url).to eq("pan.png")
+    end
   end
 
   describe "associations" do
