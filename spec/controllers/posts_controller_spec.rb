ef0cde4991c754461b04742c471c6f4314659966
John Simoni <email": null>
2017-10-29 12:44:16 -0700

    Added posts controller spec

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+  describe "ingredients/posts/index" do
+    let(:ingredient) { create(:ingredient) }
+    before(:each) { get :index, params: { ingredient_id: ingredient.id} }
+    it "renders ingredients_posts template" do
+      expect(response).to render_template(:index)
+    end
+    it "assigns an ingredient instance variable" do
+      expect(assigns[:ingredient]).to eq(ingredient)
+    end
+  end
+end
