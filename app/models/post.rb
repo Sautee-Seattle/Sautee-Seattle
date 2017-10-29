35e19ec2ea46a65250c175997305e6a2efc4ca3a
John Simoni <email": null>
2017-10-29 15:13:22 -0700

    Added convenience methods to posts  - (is_recipe? and is_location?)  - Add default values for urls

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,22 @@ class Post < ApplicationRecord
   has_many :ingredients, through: :ingredients_posts
   belongs_to :user
 
-  validates :post_type, :title, :body, presence: true
+  validates :post_type, :title, :body, :url, presence: true
   validates :title, uniqueness: {scope: :post_type}
+
+  def is_recipe?
+    post_type == "recipe"
+  end
+
+  def is_location?
+    post_type == "location"
+  end
+
+  before_validation do
+    if is_recipe?
+      self.url = "pan.png"
+    else
+      self.url = "#"
+    end
+  end
 end
