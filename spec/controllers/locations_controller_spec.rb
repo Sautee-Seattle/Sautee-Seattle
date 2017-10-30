58228053a9da3c1b2e61bb54e6edb5ef72e1fedc
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-29 18:22:44 -0700

    added 4 passing rspecs using capybara to locations controller

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -1,25 +1,31 @@
 require 'rails_helper'
+require 'capybara'
 
-RSpec.describe LocationsController, type: :controller do
+RSpec.describe LocationsController, type: :feature do
   let!(:ingredient) {Ingredient.create!(name: "Carrot")}
   let!(:user) {User.create!(username: "Bob", email: "bob@bob.com", password: "bob", bio: "Bob things.")}
   let!(:post) {Post.create!(post_type: "location", title: "Post number 1", body: "This is post 1's body!", url: "#", user: user)}
 
   context "locations#index" do
-    it 'renders the :index page successfully' do
-      get "ingredients/#{ingredient.id}/locations"
-      expect(response).to render_template :index
+    it ":index has expected content" do
+      visit("/ingredients/#{ingredient.id}/locations")
+      expect(page).to have_content 'More places to go...'
     end
-    it 'returns a status of 200 successfully'
+    it "renders the :index page successfully" do
+      visit("/ingredients/#{ingredient.id}/locations")
+      expect(page).to have_current_path(ingredient_locations_path(ingredient.id))
+    end
   end
 
-  context "locations#index" do
-    it 'renders the :index page successfully'
-    it 'returns a status of 200 successfully'
+  context "locations#new" do
+    it ":new has expected content" do
+      visit("/ingredients/#{ingredient.id}/posts/#{post.id}/locations/new")
+      expect(page).to have_content 'Share Location'
+    end
+    it "renders the :new page successfully" do
+      visit("/ingredients/#{ingredient.id}/posts/#{post.id}/locations/new")
+      expect(page).to have_current_path(new_ingredient_post_location_path(ingredient.id, post.id))
+    end
   end
-
-
-
-
 
 end
