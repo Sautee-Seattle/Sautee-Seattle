9ca3770b5ea7ab8ae53caa56272f0bb88d8b7602
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-30 13:36:49 -0700

    added 2 passing tests to ingredients controller

--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -5,13 +5,17 @@ RSpec.describe IngredientsController, type: :controller do
   let!(:user) {User.create!(username: "Bob", email: "bob@bob.com", password: "bob", bio: "Bob things.")}
   let!(:post) {Post.create!(post_type: "location", title: "Post number 1", body: "This is post 1's body!", url: "#", user: user)}
 
-  describe "ingredients#show" do
+  context "ingredients#show" do
+    it 'renders the ingredients :show page when successful' do
+      get :show, params: {id: ingredient.id}
+      expect(response).to render_template :show
+    end
     it "returns status of 200 if successful" do
       get :show, params: {id: ingredient.id}
       expect(response.status).to eq 200
     end
-
     it "assigns an ingredient instance variable" do
+      get :show, params: {id: ingredient.id}
       expect(assigns[:ingredient].kind_of?(Ingredient)).to eq(true)
     end
   end
