514bdf0cd700c69846426f9a927101ec64d79246
Marlo Ito <email": null>
2017-10-29 17:12:00 -0700

    adds assets for heroku -- hopefully

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,14 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module SauteeSeattle
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
