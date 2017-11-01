feae60ee4eac53b1bdfe42fd6527ffc7a99aa7a4
John Simoni <email": null>
2017-10-31 18:14:21 -0700

    working on page overlay

--- app/assets/javascripts/overlay.js
+++ app/assets/javascripts/overlay.js
@@ -1,3 +1,11 @@
-function loadingAnimation() {
-  
-}
+// function loadingAnimation() {
+//   var overLay = document.getElementById('overlay');
+//   overLay.style.display = 'none';
+// };
+
+// window.onload = setTimeout(loadingAnimation, 4000);
+
+$(()=>{
+  $('main').fadeIn();
+  $('#home-page').fadeIn(2500);
+});
