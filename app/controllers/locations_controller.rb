e1c6209ee9ca4f5be2d84a7c7aedf3997d84a876
Blaine Anderson <BlaineA97@Gmail.com>
2017-11-02 08:53:57 -0700

    functional

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -30,10 +30,12 @@ class LocationsController < ApplicationController
   end
 
   def show
+    location = params[:id]
+    post = Post.find(location)
+    address = post.body
+    address.gsub!(/[\s]/, '+')
 
-
-
-    uri = URI("https://maps.googleapis.com/maps/api/geocode/json?address=44th+Ave+SW+&+SW+Alaska+St,+Seattle,+WA&key=#{ENV['GOOGLE_GEOCODING_KEY']}")
+    uri = URI("https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{ENV['GOOGLE_GEOCODING_KEY']}")
     #
     # uri = URI("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=#{ENV['GOOGLE_GEOCODING_KEY']}")
 
