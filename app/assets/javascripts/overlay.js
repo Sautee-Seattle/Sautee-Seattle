71f8d40cad88f6967e2092fb066e43dd313880e5
Mandil <email": null>
2017-11-01 17:20:52 -0700

    main page spinner

--- app/assets/javascripts/overlay.js
+++ app/assets/javascripts/overlay.js
@@ -1,11 +1,31 @@
-// function loadingAnimation() {
-//   var overLay = document.getElementById('overlay');
-//   overLay.style.display = 'none';
-// };
-
-// window.onload = setTimeout(loadingAnimation, 4000);
-
+// Wrap every letter in a span
 $(()=>{
-  $('main').fadeIn();
-  $('#home-page').fadeIn(2500);
-});
+  $('.ml9 .letters').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
+
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml9 .letter',
+      scale: [0, 1],
+      duration: 1500,
+      elasticity: 600,
+      delay: function(el, i) {
+        return 45 * (i+1)
+      }
+    }).add({
+      targets: '.ml9',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
+
+    function loadingAnimation() {
+          var overlay = document.getElementById("spinner");
+          overlay.style.display = 'none';
+          $('main').fadeIn(2500);
+        };
+        window.onload = setTimeout(loadingAnimation, 4000);
+        //     $('#front-page').fadeIn();
+})
