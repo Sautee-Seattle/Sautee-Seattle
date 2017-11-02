194cef0317bbf1d13fe8550642ab707878e3c9ff
Mandil <email": null>
2017-11-01 17:36:45 -0700

    fadein on the right time

--- app/assets/javascripts/overlay.js
+++ app/assets/javascripts/overlay.js
@@ -5,27 +5,27 @@ $(()=>{
   });
 
   anime.timeline({loop: true})
-    .add({
-      targets: '.ml9 .letter',
-      scale: [0, 1],
-      duration: 1500,
-      elasticity: 600,
-      delay: function(el, i) {
-        return 45 * (i+1)
-      }
-    }).add({
-      targets: '.ml9',
-      opacity: 0,
-      duration: 1000,
-      easing: "easeOutExpo",
-      delay: 1000
-    });
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: function(el, i) {
+      return 45 * (i+1)
+    }
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
 
-    function loadingAnimation() {
-          var overlay = document.getElementById("spinner");
-          overlay.style.display = 'none';
-          $('main').fadeIn(2500);
-        };
-        window.onload = setTimeout(loadingAnimation, 4000);
-        //     $('#front-page').fadeIn();
+  function loadingAnimation() {
+    var overlay = document.getElementById("spinner");
+    overlay.style.display = 'none';
+    $('#home-page').fadeIn(2500);
+    $('.wrapper').fadeIn(2500);
+  };
+  window.onload = setTimeout(loadingAnimation, 2500);
 })
